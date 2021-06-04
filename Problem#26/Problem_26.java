b31f7acf1fd78f2a2a6773249a20e69009072981
Pranshu1902 <70687348+Pranshu1902@users.noreply.github.com>
2021-06-05 01:17:25 +0530

    Create Problem_26.java

---
+++ Problem#26/Problem_26.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Problem_26{
+    public static void main (String[] args){
+        System.out.println("CS is fun at Google");
+        List<Integer> input = new ArrayList<Integer>();
+        input.add(10);input.add(20);input.add(30);input.add(40);input.add(50);input.add(60);
+        List<Integer> result = Solution(input, 3);
+        for (int i : result){
+            System.out.print(i+" ");
+        }
+    }
+    public static List<Integer> Solution(List<Integer> inp, int k){
+        int i = inp.size()-k;
+        while(i>=0){
+            inp.remove(i);
+            i-=k;
+        }
+        return inp;
+    }
+}
