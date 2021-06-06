29b67ee67c8047bec35e5e93cd11c38e0fcad128
Pranshu1902 <70687348+Pranshu1902@users.noreply.github.com>
2021-06-06 23:23:41 +0530

    Update Problem_26.java

--- Problem#26/Problem_26.java
+++ Problem#26/Problem_26.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Problem_26{
     public static void main (String[] args){
-        System.out.println("CS is fun at Google");
         List<Integer> input = new ArrayList<Integer>();
         input.add(10);input.add(20);input.add(30);input.add(40);input.add(50);input.add(60);
         List<Integer> result = Solution(input, 3);
